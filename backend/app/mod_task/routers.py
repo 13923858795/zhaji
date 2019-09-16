fc5b1686b87f2183ca3d146c64ecbad18ce71dfc
Uther <625124155@qq.com>
2019-09-16 17:08:25 +0800

    修复  路由错误

--- backend/app/mod_task/routers.py
+++ backend/app/mod_task/routers.py
@@ -12,33 +12,11 @@ from app.mod_task.tasks import (
 
 bp = Blueprint("mod_task", __name__)
 
-# MONGODB_DB = os.environ.get("MONGODB_DB", "quatek_web_app")
-# MONGODB_HOST = os.environ.get("MONGODB_HOST", "127.0.0.1")
-# MONGODB_PORT = os.environ.get("MONGODB_PORT", 27017)
-# client = MongoClient(MONGODB_HOST, MONGODB_PORT)
-# db = client[MONGODB_DB]
-#
-#
-# @bp.route("/connect_status", methods=["GET"])
-# def connect_status():
-#
-#     db_every = [i for i in db.log_every.find()]
-#     if db_every:
-#         log_every = db_every[0]['value']
-#     else:
-#         log_every = 60*2
-#     """ 获取所有的 设备通讯时间戳 """
-#     log_data = {}
-#     for i in db.logs.find():
-#         log_data[int(i["mc_client_id"])] = i["connect_time"]
-#     """获取所有的 设备id"""
-#     resp = []
-#     for i in db.gate.find():
-#         dev_id = int(i['mc_id'])
-#         log_time = log_data.get(dev_id, False)
-#         if log_time and time.time() - log_time < log_every:
-#             resp.append(dev_id)
-#     return jsonify(resp), {"Content-Type": "application/json"}
+MONGODB_DB = os.environ.get("MONGODB_DB", "quatek_web_app")
+MONGODB_HOST = os.environ.get("MONGODB_HOST", "127.0.0.1")
+MONGODB_PORT = os.environ.get("MONGODB_PORT", 27017)
+client = MongoClient(MONGODB_HOST, MONGODB_PORT)
+db = client[MONGODB_DB]
 
 
 @bp.route("/task-interval", methods=["GET"])
